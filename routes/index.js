9a74769c855b050bba49bef5f86cdee386ca4eb8
Oleg Demidov <demidov@wad-jet.ru>
2019-07-01 03:42:02 +0300

    Issue 3: Implement authorization check for operations using the passportjs google oauth strategy and creating new users after the first authorization.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', 
+  function(req, res, next) {
+    if (util.isNullOrUndefined(req.user))
+      return res.redirect('/auth/google');
+    next();
+  },
+  function(req, res, next) {
+    res.render('index', { user: req.user, title: 'User profile' });
+  });
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
