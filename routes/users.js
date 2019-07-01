3443698a0c55597f8ff5156fe7e83446ea626660
Oleg Demidov <demidov@wad-jet.ru>
2019-07-01 03:53:20 +0300

    Issue 4: Implemented a users router operations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/levelIncrement/:id', async function(req, res, next) {
+	try {
+		var result = await user.levelIncrement(req.params.id);
+		res.status(200);
+		res.json({ level: result });
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/levelDecrement/:id', async function(req, res, next) {
+	try {
+		var result = await user.levelDecrement(req.params.id);
+		res.status(200);
+		res.json({ level: result });
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/ratingIncrement/:id', async function(req, res, next) {
+  try {
+		var result = await user.ratingIncrement(req.params.id);
+		res.status(200);
+		res.json({ rating: result });
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/ratingDecrement/:id', async function(req, res, next) {
+  try {
+		var result = await user.ratingDecrement(req.params.id);
+		res.status(200);
+		res.json({ rating: result });
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/findNearestUser/:rating/:level', async function(req, res, next) {
+  try {
+		var result = await user.findNearestUser(req.user.id, req.params.rating, req.params.level);
+		res.status(200);
+		res.json(result);
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
